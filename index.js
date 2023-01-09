f0ee360444ef5f05e8bf97146934bcf0035268ce
Thien Nguyen <thien.x.nguyen20@gmail.com>
2023-01-08 22:47:13 -0700

    Need to add Barba.js for page transistions

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const backButton = document.querySelector(".back-button");
 
-backButton.addEventListener("click", function (event) {
-  event.preventDefault(); // prevent the link from navigating away from the page
-
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
